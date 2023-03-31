3c9abb10562b82dad329929f8bee0c71aa86af02
khanhdq <khanh.dang@neopay.vn>
2023-03-31 23:33:15 +0700

    docs: update readme file

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,5 +1,3 @@
-// See at https://viblo.asia/p/nang-cao-chat-luong-code-va-hieu-qua-lam-viec-nhom-voi-husky-lint-staged-commitlint-4dbZNnMnZYM
-// Example: https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional
 module.exports = {
 	extends: ["@commitlint/config-conventional"],
 	rules: {
