352b91deec90433721a3f37b843743d08d1b5ccb
khanhdq <khanh.dang@neopay.vn>
2023-04-01 15:04:08 +0700

    style: update eslint, prettier

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,10 +1,23 @@
 module.exports = {
-	extends: ["@commitlint/config-conventional"],
-	rules: {
-		"type-enum": [
-			2,
-			"always",
-			["build", "chore", "ci", "docs", "feat", "fix", "perf", "refactor", "revert", "style", "test", "enhances"],
-		],
-	},
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'enhances',
+      ],
+    ],
+  },
 };
